3772f34b9b5d4a0e6c9fb913f1a508fb9bb8288a
Joachim Bertrand <j.bertrand0032@gmail.com>
2018-12-19 21:18:28 +0100

    retouche component Boulle

--- calender/src/Components/boulle/index.js
+++ calender/src/Components/boulle/index.js
@@ -6,22 +6,22 @@ class Boulle extends Component{
         this.state={
             lienMusique: "",
         }
-        this.handleChange = this.handleChange.bind(this);    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this)    
     }
     
 handleChange(event){
     this.setState({lienMusique :event.target.value});
 }
-    
-    componentDidUpdate(){
-        //this.props
-        //renvoyer le state au parent (Liste boulles)
-    }
 
+handleClick(elem){
+    this.props.activateUrl(elem);
+}
+<Boulle recupURL= recupURL()/>
 
     render(){
     const BoulleAdd = <li><label>Inserez un lien youtube<input type="text" value={this.state.value} onChange={this.handleChange}></input>
-    </label><input type="submit" value="Valider" onClick={}> </li>;
+    </label><input type="submit" value="Valider" onClick={() =>this.handleClick(this.state.lienMusique)}> </li>;
 
         return(
             {BoulleAdd}
