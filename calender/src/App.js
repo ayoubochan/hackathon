53d2d81645b1f0466e4348090cfbbd486ba6cd22
Carolina <carolinatirado92@gmail.com>
2018-12-19 21:38:11 +0100

    Correction index.js et adaptation du css

--- calender/src/App.js
+++ calender/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Boule from './Components/boulle/index';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Boule />
       </div>
     );
   }
--- calender/src/Components/boulle/index.js
+++ calender/src/Components/boulle/index.js
@@ -1,32 +1,51 @@
-import React, {Component} from react;
+import React, { Component } from 'react';
+import './index.css'
 
-class Boulle extends Component{
-    constructor(props){
-        super.props();
-        this.state={
+class Boule extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
             lienMusique: "",
+            open: false
         }
         this.handleChange = this.handleChange.bind(this);
-        this.handleClick = this.handleClick.bind(this)    
+        this.handleClick = this.handleClick.bind(this)
     }
-    
-handleChange(event){
-    this.setState({lienMusique :event.target.value});
-}
 
-handleClick(elem){
-    this.props.activateUrl(elem);
-}
-<Boulle recupURL= recupURL()/>
+    handleChange(event) {
+        this.setState({ lienMusique: event.target.value });
+    }
 
-    render(){
-    const BoulleAdd = <li><label>Inserez un lien youtube<input type="text" value={this.state.value} onChange={this.handleChange}></input>
-    </label><input type="submit" value="Valider" onClick={() =>this.handleClick(this.state.lienMusique)}> </li>;
+    handleClick(elem) {
+        this.props.activateUrl(elem);
+    }
 
-        return(
-            {BoulleAdd}
+    /*ouvre et ferme lorsqu'on clique*/
+    handleClickon() {
+        this.setState({
+            open: true
+        })
+    }
+    handleClickoff() {
+        this.setState({
+            open: false
+        })
+    }
+
+    render() {
+        return (
+            <div className={this.state.open ? 'cercle' : 'close'}>
+                <div className="App-cercle">
+                    <button className="btn" onClick={() => this.handleClickoff()}>Press</button>
+                    <ul className="url">
+                        <li><label>Inserez un lien youtube<input type="text" value={this.state.value} onChange={this.handleChange} />
+                        </label><input type="submit" value="Valider" onClick={() => this.handleClick(this.state.lienMusique)} /> </li>
+                    </ul>
+                </div>
+                <button className="btn1" onClick={() => this.handleClickon()}>Press</button>
+            </div>
         );
     }
 }
 
-export default Boulle
+export default Boule;
