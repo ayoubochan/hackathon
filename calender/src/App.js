6fdfff25b8782909aa92e508565da0b86dd0e15d
Carolina <carolinatirado92@gmail.com>
2018-12-19 19:18:55 +0100

    Component et css du popup

--- calender/src/App.js
+++ calender/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Popup from './components/Popup/index';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Popup />
       </div>
     );
   }
