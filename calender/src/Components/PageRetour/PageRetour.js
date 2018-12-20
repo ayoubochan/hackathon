182f0425f34fa28143b397d532a1fb21ee322dc5
Joachim Bertrand <j.bertrand0032@gmail.com>
2018-12-20 14:05:55 +0100

    creation page retour

---
+++ calender/src/Components/PageRetour/PageRetour.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class PageRetour extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            liensYoutube: []
+        }
+    }
+    
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
