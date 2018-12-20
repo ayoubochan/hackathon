a1e5a8e6ba24616b688051e36d4430ba72572d59
Ayoub Ochan <ayoub.ochan13@gmail.com>
2018-12-20 11:36:34 +0100

    rajout des dates

--- calender/src/Components/BouleList/index.js
+++ calender/src/Components/BouleList/index.js
@@ -2,38 +2,45 @@ import React, {Component} from "react"
 import Boulle from "../../Components/boulle/index"
 
 const Boules = [
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {}
+    {date: "01/12"},
+    {date: "02/12"},
+    {date: "03/12"},
+    {date: "04/12"},
+    {date: "05/12"},
+    {date: "06/12"},
+    {date: "07/12"},
+    {date: "08/12"},
+    {date: "09/12"},
+    {date: "10/12"},
+    {date: "11/12"},
+    {date: "12/12"},
+    {date: "13/12"},
+    {date: "14/12"},
+    {date: "15/12"},
+    {date: "16/12"},
+    {date: "17/12"},
+    {date: "18/12"},
+    {date: "19/12"},
+    {date: "20/12"},
+    {date: "21/12"},
+    {date: "22/12"},
+    {date: "23/12"},
+    {date: "24/12"}
 ]
 
 class BouleList extends Component{
     constructor(props){
         super(props);
         this.state = {
-            youtubeLink: []
+            youtubeLink: [],
+            date: 1
         }
+    }
+
+    dateChange(ev){
+        this.setState({
+            date: this.state.date + 1
+        })
     }
 
     RecupURL(elem){
@@ -46,7 +53,7 @@ class BouleList extends Component{
         return(
             <ul>
                 {Boules.map((elem, index) => (
-                <Boulle key={index} activateUrl={(elem) => this.RecupURL(elem)} />
+                <Boulle key={index} date={elem.date} activateUrl={(elem) => this.RecupURL(elem)} activateChange={(ev) => this.dateChange(ev)} />
             ))}
                
             </ul>
