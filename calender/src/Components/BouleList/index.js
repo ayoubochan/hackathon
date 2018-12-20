4a95c3dfc82b8c747d1a4808576eac3e37a5c669
Ayoub Ochan <ayoub.ochan13@gmail.com>
2018-12-20 17:02:53 +0100

    jtirusj

--- calender/src/Components/BouleList/index.js
+++ calender/src/Components/BouleList/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import Boulle from "../../Components/boulle/index"
 import GenerateRadioButton from "../../Components/bouttonGenerRadio/index"
 
@@ -6,34 +6,34 @@ let table = []
 let table2 = []
 
 const Boules = [
-    {date: "01/12"},
-    {date: "02/12"},
-    {date: "03/12"},
-    {date: "04/12"},
-    {date: "05/12"},
-    {date: "06/12"},
-    {date: "07/12"},
-    {date: "08/12"},
-    {date: "09/12"},
-    {date: "10/12"},
-    {date: "11/12"},
-    {date: "12/12"},
-    {date: "13/12"},
-    {date: "14/12"},
-    {date: "15/12"},
-    {date: "16/12"},
-    {date: "17/12"},
-    {date: "18/12"},
-    {date: "19/12"},
-    {date: "20/12"},
-    {date: "21/12"},
-    {date: "22/12"},
-    {date: "23/12"},
-    {date: "24/12"}
+    { date: "01/12" },
+    { date: "02/12" },
+    { date: "03/12" },
+    { date: "04/12" },
+    { date: "05/12" },
+    { date: "06/12" },
+    { date: "07/12" },
+    { date: "08/12" },
+    { date: "09/12" },
+    { date: "10/12" },
+    { date: "11/12" },
+    { date: "12/12" },
+    { date: "13/12" },
+    { date: "14/12" },
+    { date: "15/12" },
+    { date: "16/12" },
+    { date: "17/12" },
+    { date: "18/12" },
+    { date: "19/12" },
+    { date: "20/12" },
+    { date: "21/12" },
+    { date: "22/12" },
+    { date: "23/12" },
+    { date: "24/12" }
 ]
 
-class BouleList extends Component{
-    constructor(props){
+class BouleList extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             youtubeLink: [],
@@ -43,21 +43,21 @@ class BouleList extends Component{
         }
     }
 
-    dateChange(ev){
+    dateChange(ev) {
         this.setState({
             debut: this.state.debut + 1,
             fin: this.state.fin + 1
         })
     }
 
-    recupURL(elem){
+    recupURL(elem) {
         table.push(elem)
         this.setState({
             youtubeLink: table
         })
     }
 
-    stockLink(elem){
+    stockLink(elem) {
         table2.push(elem)
         this.setState({
             shareLink: table2
@@ -65,15 +65,16 @@ class BouleList extends Component{
         console.log(this.state.shareLink)
     }
 
-    render(){
-        return(
+    render() {
+        const { users } = this.state;
+        return (
             <ul>
                 {Boules.slice(this.state.debut, this.state.fin).map((elem, index) => (
-                <Boulle key={index} date={elem.date} activateUrl={(elem) => this.recupURL(elem)} activateChange={(ev) => this.dateChange(ev)} />
-            ))}
+                    <Boulle key={index} date={elem.date} activateUrl={(elem) => this.recupURL(elem)} activateChange={(ev) => this.dateChange(ev)} />
+                ))}
 
-                <GenerateRadioButton activateShare={(elem) => this.stockLink(elem)}/>
-               
+                <GenerateRadioButton activateShare={(elem) => this.stockLink(elem)} />
+
             </ul>
         )
     }
