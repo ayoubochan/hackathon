55c60541fb9b3accab57114a5844ad847b9dec8d
Ayoub Ochan <ayoub.ochan13@gmail.com>
2018-12-20 10:27:39 +0100

    modif de boulle

--- calender/src/Components/boulle/index.js
+++ calender/src/Components/boulle/index.js
@@ -20,32 +20,13 @@ class Boule extends Component {
         this.props.activateUrl(elem);
     }
 
-    /*ouvre et ferme lorsqu'on clique*/
-    handleClickon() {
-        this.setState({
-            open: true
-        })
-    }
-    handleClickoff() {
-        this.setState({
-            open: false
-        })
-    }
 
     render() {
         return (
-            <div className={this.state.open ? 'cercle' : 'close'}>
-                <div className="App-cercle">
-                    <button className="btn" onClick={() => this.handleClickoff()}>Press</button>
-                    <div>
-                        <ul className="url">
+            
                             <li><label>Inserez un lien youtube<input type="text" value={this.state.value} onChange={this.handleChange} />
                             </label><input type="submit" value="Valider" onClick={() => this.handleClick(this.state.lienMusique)} /> </li>
-                        </ul>
-                    </div>
-                </div>
-                <button className="btn1" onClick={() => this.handleClickon()}>Press</button>
-            </div>
+                      
         );
     }
 }
