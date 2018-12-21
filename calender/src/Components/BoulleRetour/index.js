b3fd3c6caaa2dbba47e7f68b43ceae9d89d03469
Carolina <carolinatirado92@gmail.com>
2018-12-21 02:23:00 +0100

    ID_partage s'adapte à la requête sql

--- calender/src/Components/BoulleRetour/index.js
+++ calender/src/Components/BoulleRetour/index.js
@@ -1,26 +1,44 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import "./index.css"
 
-class BoulleRetour extends Component{
-    constructor(props){
+class BoulleRetour extends Component {
+    constructor(props) {
         super(props)
-        this.state={
-            reponse: '',
-            date:'',
-            lienMusique:''
+        this.state = {
+            id: 1,
+            date: '',
+            youtube: [],
+            number: 0
         }
     }
 
-    transfer(ev){
-        this.props.activateChange(ev)
+    componentDidMount() {
+        fetch(`http://localhost:4000/api/calendrier/youtube?ID_partagelink=${this.state.id}`)
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    youtube: data.data,
+                })
+                console.log(data.data)
+            })
     }
 
-    render(){
-        return(
+    transfer(ev) {
+        this.props.activateChange(ev)
+        this.setState({
+            number: this.state.number + 1
+        })
+    }
+
+    render() {
+        const video = this.state.youtube.map((elem, index) => <div
+            key={index} rep={elem.Reponse}> Vidéo : {elem.Link} </div>);
+        return (
             <li><span>{this.props.date}</span>
-                <span>Donne ta réponsse</span>
-                    <input type="text" onChange={this.state.reponse} />
-                    <button onClick={(ev) => this.transfer(ev)}>Ok</button>
+                {video[this.state.number]}
+                <span>Donne ta réponse</span>
+                <input type="text" onChange={this.state.reponse} />
+                <button onClick={(ev) => this.transfer(ev)}>Ok</button>
             </li>
         )
     }
